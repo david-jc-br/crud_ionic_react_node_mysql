83be86643c4589c57d41ca05b5402fac3a7fa913
David de Jesus Costa <73839667+david-jc-br@users.noreply.github.com>
2023-05-12 12:17:26 -0300

    build

---
+++ front_end/build/static/js/841.4286a619.chunk.js
@@ -0,0 +1,3 @@
+/*! For license information please see 841.4286a619.chunk.js.LICENSE.txt */
+"use strict";(globalThis.webpackChunkfront_end=globalThis.webpackChunkfront_end||[]).push([[841],{841:(e,t,n)=>{n.r(t),n.d(t,{startStatusTap:()=>a});var o=n(554),r=n(743),s=n(811);const a=()=>{const e=window;e.addEventListener("statusTap",(()=>{(0,o.wj)((()=>{const t=e.innerWidth,n=e.innerHeight,a=document.elementFromPoint(t/2,n/2);if(!a)return;const i=(0,r.a)(a);i&&new Promise((e=>(0,s.c)(i,e))).then((()=>{(0,o.Iu)((async()=>{i.style.setProperty("--overflow","hidden"),await(0,r.s)(i,300),i.style.removeProperty("--overflow")}))}))}))}))}}}]);
+//# sourceMappingURL=841.4286a619.chunk.js.map
